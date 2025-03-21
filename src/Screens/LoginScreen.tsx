f8f5248bf333146cfd17090539ee5f3233a30b5a
Naduladisanayaka494 <disanayakanadula494@gmail.com>
2025-03-21 09:36:30 +0530

    modify: api url modified to current network

--- src/Screens/LoginScreen.tsx
+++ src/Screens/LoginScreen.tsx
@@ -37,16 +37,13 @@ const LoginScreen = () => {
     setLoading(true);
 
     try {
-      const response = await fetch(
-        'http://192.168.8.102:5000/api/auth/login',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({email, password}),
+      const response = await fetch('http://192.168.1.10:5000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      );
+        body: JSON.stringify({email, password}),
+      });
 
       const data = await response.json();
 
